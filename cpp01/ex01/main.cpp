7871311d41532dcbc2cd956a64c01193b694885b
Ko en Cheong <kcheong@u90z05s01.42kl.edu.my>
2023-11-08 16:38:48 +0800

    Trying cpp01/ex01

---
+++ cpp01/ex01/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+class Zombie
+{
+	public:
+		Zombie();
+		~Zombie();
+		void	announce(void);
+		void	setName(std::string name);
+
+	private:
+		std::string name;
+};
+
+Zombie::Zombie()
+{
+	std::cout << this->name << " constructor called." << std::endl;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << this->name << " destructor called." << std::endl;
+}
+
+void	Zombie::announce()
+{
+	std::cout << this->name;
+	std::cout << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void	Zombie::setName(std::string name)
+{
+	this->name = name;
+}
+
+Zombie* zombieHorde(int N, std::string name)
+{
+	Zombie *zombieHorde = new Zombie[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		zombieHorde[i].setName(name);
+	}
+	return (zombieHorde);
+}
+
+int main()
+{
+	Zombie *horde;
+	
+	horde = zombieHorde(5, "Koen");
+	for (int i = 0; i < 5; i++)
+	{
+		horde[i].announce();
+	}
+
+}
